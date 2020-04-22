26ccb6e3547a94a27e7c12b148474c5877fc3b86
Sanjay Paul Charles <sanjaypaulcharles98@gmail.com>
2020-04-22 18:49:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 function iterativeLog(array){
-array.forEach(el,index,array)
+array.forEach((el,index,array)=>{
+  console.log(`${index}: ${element}`)
+})
 }
 function iterate(callback){
   var arr=[1,2,3,4,5]
